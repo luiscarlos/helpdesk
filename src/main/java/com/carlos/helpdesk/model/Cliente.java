9df94ff47aa85b25b200a0f3c6669140c49e416c
luiscarlos <luiscarlos8pe@gmail.com>
2023-07-27 19:02:24 -0300

    crud para cliente implementado

--- src/main/java/com/carlos/helpdesk/model/Cliente.java
+++ src/main/java/com/carlos/helpdesk/model/Cliente.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.carlos.helpdesk.dtos.ClienteDTO;
 import com.carlos.helpdesk.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -27,6 +28,17 @@ public class Cliente extends Pessoa {
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 
