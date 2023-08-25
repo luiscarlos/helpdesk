61a4b9658f58ad0657a720a827fd7919d20b15ec
luiscarlos <luiscarlos8pe@gmail.com>
2023-08-25 20:49:26 -0300

    Autenticacao com JWT

---
+++ src/main/java/com/carlos/helpdesk/services/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.carlos.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.carlos.helpdesk.model.Pessoa;
+import com.carlos.helpdesk.repository.PessoaRepository;
+import com.carlos.helpdesk.security.UserSS;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	private PessoaRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<Pessoa> user = repository.findByEmail(email);
+		if(user.isPresent()) {
+			return new UserSS(user.get().getId(), user.get().getEmail(), user.get().getSenha(), user.get().getPerfis());
+		}
+		throw new UsernameNotFoundException(email);
+	}
+
+}
